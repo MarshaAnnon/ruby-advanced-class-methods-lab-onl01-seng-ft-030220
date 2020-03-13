8914cdaac5747381c363009586755d9278e1c9cc
Marsha Annon <marshaannon@gmail.com>
2020-03-13 21:16:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ class Song
   def self.new_from_filename(name)
     song = self.new
     artist_name = self.new
-    song.name = (name.split(" - ")[1].delete_suffix(".mp3"))
+    song.name = (name.split(" - ")[1].delete!(".mp3"))
     song.artist_name = (name.split(" - ")[0])
     song
 

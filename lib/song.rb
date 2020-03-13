2acccab30e20600f1d784412ec3d0da6764e6458
Marsha Annon <marshaannon@gmail.com>
2020-03-13 20:18:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,8 +33,8 @@ class Song
   end
 
   def self.find_or_create_by_name(song)
-    if self.find_by_name(name) || self.create_by_name(name)
-    else self.create.new
+   self.find_by_name(name) || self.create_by_name(name)
+  
     end
 
 
